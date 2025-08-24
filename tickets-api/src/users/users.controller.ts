d63b01718b30fc1fb22c9ba1ee5bad7c7baf4e53
Daniel Marques <daniel.marques142@gmail.com>
2025-08-24 14:50:01 -0300

    feat: add intercerptors for users

--- tickets-api/src/users/users.controller.ts
+++ tickets-api/src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Post, Get, Patch, Delete, Param, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  Get,
+  Patch,
+  Delete,
+  Param,
+  Query,
+  UseInterceptors,
+  ClassSerializerInterceptor,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
@@ -12,6 +23,7 @@ export class UsersController {
     return this.usersService.create(body);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
