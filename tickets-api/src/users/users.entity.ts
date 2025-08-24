34906c28c6848ebc0faa4cb1b6819ec03bccfb5c
Daniel Marques <daniel.marques142@gmail.com>
2025-08-24 12:12:19 -0300

    feat: create user

--- tickets-api/src/users/users.entity.ts
+++ tickets-api/src/users/users.entity.ts
@@ -1,4 +1,9 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class User {
@@ -11,9 +16,9 @@ export class User {
   @Column()
   password: string;
 
-  @Column()
+  @CreateDateColumn()
   createdAt: Date;
 
-  @Column()
+  @CreateDateColumn()
   updatedAt: Date;
 }
--- tickets-api/src/users/users.service.ts
+++ tickets-api/src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private repo: Repository<User>;
+
+  constructor(@InjectRepository(User) repo: Repository<User>) {
+    this.repo = repo;
+  }
+
+  create(email: string, password: string) {
+    const user = this.repo.create({
+      email,
+      password,
+    });
+
+    return this.repo.save(user);
+  }
+}
