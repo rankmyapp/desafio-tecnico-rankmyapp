d5d4a65a8581f6c7df1f18394bac9d6aa5e114c2
Daniel Marques <daniel.marques142@gmail.com>
2025-08-26 17:06:25 -0300

    feat: guard tickets endpoints

--- tickets-api/src/tickets/tickets.controller.ts
+++ tickets-api/src/tickets/tickets.controller.ts
@@ -6,41 +6,49 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { CreateTicketDto } from './dtos/create-ticket.dto';
 import { BuyTicketDto } from './dtos/buy-ticket.dto';
+import { AuthGuard } from 'src/users/auth/auth.guard';
 
 @Controller('tickets')
 export class TicketsController {
   constructor(private ticketsService: TicketsService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() body: CreateTicketDto) {
     return this.ticketsService.create(body);
   }
 
   @Get()
+  @UseGuards(AuthGuard)
   findAll() {
     return this.ticketsService.find();
   }
 
   @Get('/:id')
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.ticketsService.findOne(parseInt(id));
   }
 
   @Patch('/:id')
+  @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() body: CreateTicketDto) {
     return this.ticketsService.update(parseInt(id), body);
   }
 
   @Delete('/:id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.ticketsService.remove(parseInt(id));
   }
 
   @Post('/buy')
+  @UseGuards(AuthGuard)
   buy(@Body() body: BuyTicketDto) {
     return this.ticketsService.buy(body);
   }
